acadd17bd0ce788ec5ee08b08648396b90da912d
ERIS RISO <erisriso@gmail.com>
2020-09-11 01:26:00 +0700

    update koneksi

--- config/datastores.js
+++ config/datastores.js
@@ -48,10 +48,12 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mysql',
+    url: 'mysql://sails_mariadb:sailsjs@host:localhost/sails_mariadb',
 
   },
+
+ 
 
 
 };
